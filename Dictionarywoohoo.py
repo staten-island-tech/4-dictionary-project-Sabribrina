1ab770f45164809334a825c3bad2752d74a5d0bb
Sabribrina <sabrinal117@nycstudents.net>
2026-02-11 10:15:49 -0500

    2/11/26

---
+++ Dictionarywoohoo.py
@@ -0,0 +1,6 @@
+item = {
+    "name": "Samsung 55\" 4k UHD TV",
+    "price": 429.99,
+    "department": "Televisions",
+    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+}
