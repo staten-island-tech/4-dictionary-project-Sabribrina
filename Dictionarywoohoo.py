cea07747b363b8aa332255eb82c7a63dc193c67c
Sabribrina <sabrinal117@nycstudents.net>
2026-02-12 10:13:24 -0500

    2/12/26

--- Dictionarywoohoo.py
+++ Dictionarywoohoo.py
@@ -1,6 +1,27 @@
-item = {
-    "name": "Samsung 55\" 4k UHD TV",
-    "price": 429.99,
-    "department": "Televisions",
-    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+YURI = [
+{
+    "name": "IILWTV",
+    "price": 19.99,
+    "department": "Manga",
+    "description": "Absolute peak transmigration yuri",
+},
+{
+    "name": "TGSWIIWAGAA",\
+    "price": 20.00,
+    "department": "Manga",
+    "description": "W green yuriful yuri",
 }
+]
+
+print(YURI[0]["name"])
+print(YURI[0]["price"])
+print(YURI[0]["department"])
+print(YURI[0]["description"])
+
+print(YURI[1]["name"])
+print(YURI[1]["price"])
+print(YURI[1]["department"])
+print(YURI[1]["description"])
+
+for index, item in enumerate(best_buy_items):
+    print(index, ":", item["name"])
