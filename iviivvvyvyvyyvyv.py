8d93b069c6c675cb4bb101701d9a540472292346
Sabribrina <sabrinal117@nycstudents.net>
2026-03-31 10:17:17 -0400

    3/31/26

---
+++ iviivvvyvyvyyvyv.py
@@ -0,0 +1,17 @@
+wards = {
+    "Cardiology":  ["Alice", "Bob", "Carol"],
+    "Neurology":   ["Diana", "Eve"],
+    "Orthopedics": ["Frank", "Grace", "Hank"],
+    "Oncology":    ["Ivy", "Bob"]
+}
+
+def field (wards):
+    staff = {}
+    for dept in wards:
+        if doc not in staff:
+            staff += name
+        else:
+            field[dept['']]
+    for dept, doc in wards.items():
+            name = doc[]
+            print(name, dept)
--- sushiowo.py
+++ sushiowo.py
@@ -15,11 +15,14 @@ def receipt (orders):
     the_receipt = {}
     for sushi in orders:
         if sushi['name'] in the_receipt:
-            print("yay", the_receipt)
+            the_receipt[sushi['name']]['quantity'] += 1
         else:
             the_receipt[sushi['name']] = {
                 'price': sushi['price'],
                 'quantity': 1
             }
-
+    for sushi, value in the_receipt.items():
+        price = value['price'] * value['quantity']
+        print(sushi, value['quantity'], price)
+        
 receipt(sushi_orders)
